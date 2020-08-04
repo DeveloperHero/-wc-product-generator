8af6e94b53fdaa204acb92bf773ed5b97823deaf
ibabul <wpimtiaz@gmail.com>
2020-08-04 22:13:15 +0600

    Assets files are linked

--- includes/woo-assets.php
+++ includes/woo-assets.php
@@ -1,27 +1,30 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	wp_die();
 }
 
 /**
  * Tools submenu.
  */
-class WOO_Faker_Assets {
-	public function __construct() {
-		add_action( 'init', array( $this, 'init' ) );
-		
-		 add_action( 'admin_enqueue_scripts', [ $this, 'woofaker_enqueue_script' ] );
+class WOO_Faker_Assets
+{
+	public function __construct()
+	{
+		add_action('init', array($this, 'init'));
+
+		add_action('admin_enqueue_scripts', [$this, 'woofaker_enqueue_script']);
 	}
 
-	public function init() {
-
+	public function init()
+	{
 	}
 
-	public function woofaker_enqueue_script() {
-		wp_enqueue_script( 'woofaker' , WOOFAKER_ASSETS . '/main.js' );
+	public function woofaker_enqueue_script()
+	{
+		wp_enqueue_script('woofaker', WOOFAKER_ASSETS . '/main.js');
 
-		wp_enqueue_style( 'woofaker' , WOOFAKER_ASSETS . '/style.css' );
+		wp_enqueue_style('woofaker', WOOFAKER_ASSETS . '/style.css');
 	}
 }
 
--- templates/form.php
+++ templates/form.php
@@ -1,94 +1,94 @@
 <?php
- $_options = get_option('__woofaker_options');
+$_options = get_option('__woofaker_options');
 ?>
-<form method="post" action="<?php echo admin_url( 'tools.php?page=woofaker-product-generator' ); ?>">
+<form method="post" action="<?php echo admin_url('tools.php?page=woofaker-product-generator'); ?>">
 
 	<!-- define form fields -->
 	<table class="form-table">
 
-	</div>
-	<?php
-	if ( ! empty( $this->errors['product_type'] ) ) {
+		</div>
+		<?php
+		if (!empty($this->errors['product_type'])) {
 		?>
-		<div class="notice update-nag is-dismissible"> 
-		<?php
-		echo $this->errors['product_type'];
-	}
-	?>
-
-		<?php
-		if ( ! empty( $this->errors['product_title'] ) ) {
-			?>
-			<div class="notice update-nag is-dismissible"> 
+			<div class="notice update-nag is-dismissible">
 			<?php
-			echo $this->errors['product_title'];
+			echo $this->errors['product_type'];
 		}
-		?>
-			</div>
+			?>
 
-		<tbody>
-			<tr class="row">
-				<th scope="row">
-					<label for="product_title">
-						<?php _e( 'Product title', 'woofaker' ); ?>
-					</label>
-				</th>
-				<td>
-					<input type="text"  name="create_products_title" id="create_products_title" class="regular-text" value="<?php echo $_options['create_products_title']; ?>">
-				</td>
-			</tr>
+			<?php
+			if (!empty($this->errors['product_title'])) {
+			?>
+				<div class="notice update-nag is-dismissible">
+				<?php
+				echo $this->errors['product_title'];
+			}
+				?>
+				</div>
 
-			<tr class="row">
-				<th scope="row">
-					<label for="product_type">
-						<?php _e( 'Product type', 'woofaker' ); ?>
-					</label>
-				</th>
-				<td>
-					<input type="checkbox" <?php echo ! empty( $_options['create_simple_products'] ) && $_options['create_simple_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_simple_products" id="create_simple_products" class="regular-text" value="yes">
-					<?php _e( 'Simple product', 'woofaker' ); ?>
-					<br>
+				<tbody>
+					<tr class="row">
+						<th scope="row">
+							<label for="product_title">
+								<?php _e('Product title', 'woofaker'); ?>
+							</label>
+						</th>
+						<td>
+							<input type="text" name="create_products_title" id="create_products_title" class="regular-text" value="<?php echo $_options['create_products_title']; ?>">
+						</td>
+					</tr>
 
-					<input type="checkbox" <?php echo ! empty( $_options['create_variable_products'] ) && $_options['create_variable_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_variable_products" id="create_variable_products" class="regular-text" value="yes">
-					<?php _e( 'Variable product', 'woofaker' ); ?>
-					<br>
+					<tr class="row">
+						<th scope="row">
+							<label for="product_type">
+								<?php _e('Product type', 'woofaker'); ?>
+							</label>
+						</th>
+						<td>
+							<input type="checkbox" <?php echo !empty($_options['create_simple_products']) && $_options['create_simple_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_simple_products" id="create_simple_products" class="regular-text" value="yes">
+							<?php _e('Simple product', 'woofaker'); ?>
+							<br>
 
-					<input type="checkbox" <?php echo ! empty( $_options['create_grouped_products'] ) && $_options['create_grouped_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_grouped_products" id="create_grouped_products" class="regular-text" value="yes">
-					<?php _e( 'Grouped product', 'woofaker' ); ?>
-					<br>
+							<input type="checkbox" <?php echo !empty($_options['create_variable_products']) && $_options['create_variable_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_variable_products" id="create_variable_products" class="regular-text" value="yes">
+							<?php _e('Variable product', 'woofaker'); ?>
+							<br>
 
-					<input type="checkbox" <?php echo ! empty( $_options['create_external_products'] ) && $_options['create_external_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_external_products" id="create_external_products" class="regular-text" value="yes">
-					<?php _e( 'External product', 'woofaker' ); ?>
-					<br>
-				</td>
-			</tr>
+							<input type="checkbox" <?php echo !empty($_options['create_grouped_products']) && $_options['create_grouped_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_grouped_products" id="create_grouped_products" class="regular-text" value="yes">
+							<?php _e('Grouped product', 'woofaker'); ?>
+							<br>
+
+							<input type="checkbox" <?php echo !empty($_options['create_external_products']) && $_options['create_external_products']  == 'yes' ? "checked='checked'" : ''; ?> name="create_external_products" id="create_external_products" class="regular-text" value="yes">
+							<?php _e('External product', 'woofaker'); ?>
+							<br>
+						</td>
+					</tr>
 
 
-			<tr class="row">
-				<th scope="row">
-					<label for="name">
-						<?php _e( 'Random Image on Product', 'woofaker' ); ?>
-					</label>
-				</th>
-				<td>
-					<input type="radio" <?php echo ! empty( $_options['add_random_images'] ) && $_options['add_random_images']  == 'yes' ? "checked='checked'" : ''; ?>  name="add_random_images" id="add_random_images" class="regular-text" value="yes">
-					<?php _e( 'Yes', 'woofaker' ); ?>
-					<br>
+					<tr class="row">
+						<th scope="row">
+							<label for="name">
+								<?php _e('Random Image on Product', 'woofaker'); ?>
+							</label>
+						</th>
+						<td>
+							<input type="radio" <?php echo !empty($_options['add_random_images']) && $_options['add_random_images']  == 'yes' ? "checked='checked'" : ''; ?> name="add_random_images" id="add_random_images" class="regular-text" value="yes">
+							<?php _e('Yes', 'woofaker'); ?>
+							<br>
 
-					<input type="radio" <?php echo ! empty( $_options['add_random_images'] ) && $_options['add_random_images']  == 'no' ? "checked='checked'" : ''; ?>   name="add_random_images" id="" class="regular-text" value="no">
-					<?php _e( 'No', 'woofaker' ); ?>
-					<br>
-				</td>
-			</tr>
+							<input type="radio" <?php echo !empty($_options['add_random_images']) && $_options['add_random_images']  == 'no' ? "checked='checked'" : ''; ?> name="add_random_images" id="" class="regular-text" value="no">
+							<?php _e('No', 'woofaker'); ?>
+							<br>
+						</td>
+					</tr>
 
 	</table>
 
-	
-		
 
-			<?php wp_nonce_field( 'woofaker_submit_product_gen' ); ?>
-			<?php
-			submit_button( __( 'Submit', 'woofaker' ), 'primary', 'submit_button' );
-			
-			?>
+
+
+	<?php wp_nonce_field('woofaker_submit_product_gen'); ?>
+	<?php
+	submit_button(__('Submit', 'woofaker'), 'primary', 'submit_button');
+
+	?>
 </form>
