89a3a8e81cae44fa88e49f18650e2a59e4b9dd60
M Yakub Mizan <ym@orchestra.ltd>
2020-08-04 14:22:43 +0600

    minor fixes

--- templates/form.php
+++ templates/form.php
@@ -1,4 +1,4 @@
-<form method="post" action="<?php echo admin_url('tools.php?page=woofaker-product-generator'); ?>">
+<form method="post" action="<?php echo admin_url( 'tools.php?page=woofaker-product-generator' ); ?>">
 
 	<!-- define form fields -->
 	<table class="form-table">
@@ -6,7 +6,7 @@
 			<tr class="row">
 				<th scope="row">
 					<label for="product_title">
-						<?php _e('Prodct title', 'woofaker'); ?>
+						<?php _e( 'Product title', 'woofaker' ); ?>
 					</label>
 				</th>
 				<td>
@@ -17,24 +17,24 @@
 			<tr class="row">
 				<th scope="row">
 					<label for="product_type">
-						<?php _e('Prodct type', 'woofaker'); ?>
+						<?php _e( 'Product type', 'woofaker' ); ?>
 					</label>
 				</th>
 				<td>
-					<input type="checkbox" name="create_simple_products" id="create_simple_products" class="regular-text" value="create_simple_products">
-					<?php _e('Simple product', 'woofaker'); ?>
+					<input type="checkbox" name="create_simple_products" id="create_simple_products" class="regular-text" value="yes">
+					<?php _e( 'Simple product', 'woofaker' ); ?>
 					<br>
 
-					<input type="checkbox" name="create_variable_products" id="create_variable_products" class="regular-text" value="create_variable_products">
-					<?php _e('Variable product', 'woofaker'); ?>
+					<input type="checkbox" name="create_variable_products" id="create_variable_products" class="regular-text" value="yes">
+					<?php _e( 'Variable product', 'woofaker' ); ?>
 					<br>
 
-					<input type="checkbox" name="create_grouped_products" id="create_grouped_products" class="regular-text" value="create_grouped_products">
-					<?php _e('Grouped product', 'woofaker'); ?>
+					<input type="checkbox" name="create_grouped_products" id="create_grouped_products" class="regular-text" value="yes">
+					<?php _e( 'Grouped product', 'woofaker' ); ?>
 					<br>
 
-					<input type="checkbox" name="create_external_products" id="create_external_products" class="regular-text" value="create_external_products">
-					<?php _e('External product', 'woofaker'); ?>
+					<input type="checkbox" name="create_external_products" id="create_external_products" class="regular-text" value="yes">
+					<?php _e( 'External product', 'woofaker' ); ?>
 					<br>
 				</td>
 			</tr>
@@ -42,42 +42,47 @@
 			<tr class="row">
 				<th scope="row">
 					<label for="name">
-						<?php _e('Random Image on Product', 'woofaker'); ?>
+						<?php _e( 'Random Image on Product', 'woofaker' ); ?>
 					</label>
 				</th>
 				<td>
-					<input type="checkbox" name="add_random_images" id="add_random_images" class="regular-text" value="add_random_images">
-					<?php _e('Yes', 'woofaker'); ?>
+					<input type="radio" name="add_random_images" id="add_random_images" class="regular-text" value="add_random_images">
+					<?php _e( 'Yes', 'woofaker' ); ?>
 					<br>
 
-					<input type="checkbox" name="" id="" class="regular-text" value="">
-					<?php _e('No', 'woofaker'); ?>
+					<input type="radio" name="add_random_images" id="" class="regular-text" value="">
+					<?php _e( 'No', 'woofaker' ); ?>
 					<br>
-
 				</td>
 			</tr>
 
 	</table>
 
-	<?php if (!empty($this->errors['product_type'])) {
+	<?php
+	if ( ! empty( $this->errors['product_type'] ) ) {
+		?>
+		<div class="update-nag notice"> 
+		<?php
+		echo $this->errors['product_type'];
+	}
 	?>
-		<div class="update-nag notice"> <?php echo  $this->errors['product_type'];
-												}
-													?>
 		</div>
 
-		<?php if (!empty($this->errors['product_title'])) {
+		<?php
+		if ( ! empty( $this->errors['product_title'] ) ) {
+			?>
+			<div class="update-nag notice"> 
+			<?php
+			echo $this->errors['product_title'];
+		}
 		?>
-			<div class="update-nag notice"> <?php echo  $this->errors['product_title'];
-													}
-														?>
 			</div>
 
-			<?php wp_nonce_field('woofaker_submit_product_gen'); ?>
-			<?php submit_button(__('Submit', 'woofaker'), 'primary', 'submit_button');
+			<?php wp_nonce_field( 'woofaker_submit_product_gen' ); ?>
+			<?php
+			submit_button( __( 'Submit', 'woofaker' ), 'primary', 'submit_button' );
 			echo '<pre>';
-			print_r(get_option('__woofaker_options'));
-			echo '</pre>'; ?>
-
-
+			print_r( get_option( '__woofaker_options' ) );
+			echo '</pre>';
+			?>
 </form>
